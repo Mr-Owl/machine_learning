78a855df1a4ed148bb2dddefe8f3dbc6c7d54d2f
Mr-Owl <Mr-Owl@163.com>
2021-12-10 12:09:31 +0800

    显示图例

--- 01-matplotlib/1-hello_word.py
+++ 01-matplotlib/1-hello_word.py
@@ -8,18 +8,19 @@ mpl.rcParams["font.sans-serif"] = ["SimHei"]
 mpl.rcParams["axes.unicode_minus"] = False
 
 
-
 def main():
     # 0.准备数据
     x = range(60)
     # uniform 生成一个随机实数
     y_shanghai = [random.uniform(15, 18) for i in x]
+    y_beijing = [random.uniform(3, 10) for i in x]
 
     # 1.创建画布
     plt.figure(figsize=(20, 8), dpi=100)
 
     # 2.绘制图像
-    plt.plot(x, y_shanghai)
+    plt.plot(x, y_shanghai, label="上海")
+    plt.plot(x, y_beijing, color="r", linestyle="--", label="北京")
 
     # 2.1 添加x,y轴刻度
     # 构造x,y轴刻度标签
@@ -38,6 +39,8 @@ def main():
     plt.ylabel("温度")
     plt.title("中午11点--12点某城市温度变化图", fontsize=20)
 
+    # 显示图例  loc显示位置 best自动设置位置
+    plt.legend(loc="best")
     # 2.4 图像保存，在show之前保存
     # show之后会释放数据，就会变成一张空白图
     plt.savefig("./test.png")
