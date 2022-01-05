758cf1343684d53865b8c039d67e1bffdb71a665
Mr-Owl <Mr-Owl@163.com>
2022-01-05 19:32:32 +0800

    03 岭回归

--- 06-线性回归/02-波士顿房价预测案例.py
+++ 06-线性回归/02-波士顿房价预测案例.py
@@ -9,7 +9,7 @@
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge, RidgeCV
 from sklearn.metrics import mean_squared_error
 
 
@@ -101,7 +101,53 @@ def linear_model2():
     print("均方误差：\n", ret)
 
 
+def linear_model3():
+    """
+    线性回归：岭回归
+    sklearn.linear_model.Ridge(alpha=1.0, fit_intercept=True,solver="auto", normalize=False)
+    具有l2正则化的线性回归
+    alpha:正则化力度，也叫 λ
+    λ取值：0~1 1~10
+
+    solver:会根据数据自动选择优化方法
+    sag:如果数据集、特征都比较大，选择该随机梯度下降优化
+
+    normalize:数据是否进行标准化
+    normalize=False:可以在fit之前调用preprocessing.StandardScaler标准化数据
+
+    Ridge.coef_:回归权重
+    Ridge.intercept_:回归偏置
+    :return:
+    """
+    # 1 获取数据
+    boston = load_boston()
+    print(boston)
+    # 2 数据基本处理
+    # 2.1 分割数据
+    x_train, x_test, y_train, y_test = train_test_split(boston.data, boston.target, test_size=0.2)
+    # 3 特征工程-标准化
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.fit_transform(x_test)
+    # 4 机器学习-线性回归
+    # estimator = Ridge(alpha=1.0)
+    estimator = RidgeCV(alphas=(0.001, 0.01, 0.1, 1, 10, 100))
+    estimator.fit(x_train, y_train)
+    print("模型的偏置是：\n", estimator.intercept_)
+    print("模型的系数是：\n", estimator.coef_)
+    # 5 模型评估
+    # 5.1 预测值
+    y_pre = estimator.predict(x_test)
+    print("预测值是：\n", y_pre)
+    # 5.2 均方误差
+    ret = mean_squared_error(y_test, y_pre)
+    print("均方误差：\n", ret)
+
+
+
 if __name__ == '__main__':
     linear_model1()
     print("--------------------")
     linear_model2()
+    print("--------------------")
+    linear_model3()
