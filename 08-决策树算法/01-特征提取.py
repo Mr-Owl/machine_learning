ed42f5734b7136d8a0d14ac68f8f2142b2795309
Mr.Xing <Mr_Owl@163.com>
2022-01-13 23:16:50 +0800

    01-特征提取之tfidf文本特征提取

--- 08-决策树算法/01-特征提取.py
+++ 08-决策树算法/01-特征提取.py
@@ -1,6 +1,6 @@
 import jieba
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 """
 特征提取
@@ -138,8 +138,52 @@ def chinese_count_demo2():
     print(new_data)
 
 
+def tfidf_demo():
+    """
+    Tf-idf文本特征提取
+    TF-IDF的主要思想是：如果某个词或短语在一篇文章中出现的概率高，并且在其他文章中很少出现，则认为此词或者短语具有很好的类别区分能力，适合用来分类。
+    TF-IDF作用：用以评估一字词对于一个文件集或一个语料库中的其中一份文件的重要程度。
+    api:
+    sklearn.feature_extraction.text.TfidfVectorizer
+    公式
+    词频（term frequency，tf）指的是某一个给定的词语在该文件中出现的频率
+    逆向文档频率（inverse document frequency，idf）是一个词语普遍重要性的度量。
+    某一特定词语的idf，可以由总文件数目除以包含该词语之文件的数目，再将得到的商取以10为底的对数得到
+    tfidf = tf*idf
+    最终得出结果可以理解为重要程度。
+    举例：
+    假如一篇文章的总词语数是100个，而词语"非常"出现了5次，那么"非常"一词在该文件中的词频就是5/100=0.05。
+    而计算文件频率（IDF）的方法是以文件集的文件总数，除以出现"非常"一词的文件数。
+    所以，如果"非常"一词在1,0000份文件出现过，而文件总数是10,000,000份的话，
+    其逆向文件频率就是lg（10,000,000 / 1,0000）=3。
+    最后"非常"对于这篇文档的tf-idf的分数为0.05 * 3=0.15
+    :return:
+
+    """
+    # 获取数据
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
+    # 将原始数据转换成分好词的形式
+    text_list = []
+    for sent in data:
+        text_list.append(cut_word(sent))
+    print(text_list)
+    # 文本特征转换
+    transfer = TfidfVectorizer(stop_words=["一种", "今天"])
+    new_data = transfer.fit_transform(text_list)
+
+    # 查看特征名字
+    names = transfer.get_feature_names()
+
+    print("特征名字是：", names)
+    print(new_data.toarray())  # 转换成one-hot矩阵
+    # print(new_data)
+
+
 if __name__ == '__main__':
     # dict_demo()
     # english_count_demo()
     # chinese_count_demo()
-    chinese_count_demo2()
+    # chinese_count_demo2()
+    tfidf_demo()
